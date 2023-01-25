0437fb6de34ff5f89df312c7ca3ec8ce2a1eabd6
Feilong Ma <mafeilong@gmail.com>
2023-01-25 12:27:26 -0500

    Migrate functions to compute vertex normals from process.surface

--- src/neuroboros/surface/__init__.py
+++ src/neuroboros/surface/__init__.py
@@ -58,6 +58,20 @@ class Sphere(Surface):
         instance = cls(coords, faces)
         return instance
 
+    @property
+    def normals(self):
+        if not hasattr(self, '_normals'):
+            self._normals = compute_vertex_normals_equal_weight(
+                self.coords, self.faces)
+        return self._normals
+
+    @property
+    def normals_sine(self):
+        if not hasattr(self, '_normals_sine'):
+            self._normals_sine = compute_vertex_normals_sine_weight(
+                self.coords, self.faces)
+        return self._normals_sine
+
     def prepare_barycentric(self):
         if not hasattr(self, 'vecs'):
             f_coords = self.coords[self.faces, :]
@@ -93,3 +107,37 @@ def compute_neighbors(faces, nv=None):
         neighbors[i] = np.array(neighbors[i])
 
     return neighbors
+
+
+def compute_vertex_normals_sine_weight(coords, faces):
+    normals = np.zeros(coords.shape)
+
+    f_coords = coords[faces]
+    edges = np.roll(f_coords, 1, axis=1) - f_coords
+    del f_coords
+    edges /= np.linalg.norm(edges, axis=2, keepdims=True)
+
+    for f, ee in zip(faces, edges):
+        normals[f[0]] += np.cross(ee[0], ee[1])
+        normals[f[1]] += np.cross(ee[1], ee[2])
+        normals[f[2]] += np.cross(ee[2], ee[0])
+    normals /= np.linalg.norm(normals, axis=1, keepdims=True)
+
+    return normals
+
+
+def compute_vertex_normals_equal_weight(coords, faces):
+    normals = np.zeros(coords.shape)
+
+    f_coords = coords[faces]
+    e01 = f_coords[:, 1, :] - f_coords[:, 0, :]
+    e12 = f_coords[:, 2, :] - f_coords[:, 1, :]
+    del f_coords
+
+    face_normals = np.cross(e01, e12)
+    face_normals /= np.linalg.norm(face_normals, axis=1, keepdims=True)
+    for f, n in zip(faces, face_normals):
+        normals[f] += n
+    normals /= np.linalg.norm(normals, axis=1, keepdims=True)
+
+    return normals
