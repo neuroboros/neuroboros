3cbaecdee26f24c67ced0785f02125925b43981d
Ma Feilong <mafeilong@gmail.com>
2023-01-28 14:50:51 -0500

    Add area-related properties and update areal interpolation methods.

--- src/neuroboros/surface/__init__.py
+++ src/neuroboros/surface/__init__.py
@@ -3,7 +3,8 @@ import nibabel as nib
 from scipy.spatial import cKDTree
 
 from .properties import compute_neighbors, compute_neighbor_distances, \
-    compute_vertex_normals_equal_weight, compute_vertex_normals_sine_weight
+    compute_vertex_normals_equal_weight, compute_vertex_normals_sine_weight, \
+    compute_vertex_areas, compute_face_areas
 from .barycentric import barycentric, barycentric_vectors, barycentric_weights
 from .nnfr import nnfr
 from .areal import areal
@@ -36,6 +37,19 @@ class Surface(object):
         if not hasattr(self, '_tree'):
             self._tree = cKDTree(self.coords)
         return self._tree
+
+    @property
+    def vertex_areas(self):
+        if not hasattr(self, '_vertex_areas'):
+            self._vertex_areas = compute_vertex_areas(
+                self.coords, self.faces, self.face_areas)
+        return self._vertex_areas
+
+    @property
+    def face_areas(self):
+        if not hasattr(self, '_face_areas'):
+            self._face_areas = compute_face_areas(self.coords, self.faces)
+        return self._face_areas
 
     @property
     def v2f(self):
@@ -110,8 +124,17 @@ class Sphere(Surface):
     def nnfr(self, coords, reverse=True):
         return nnfr(self.coords, coords, reverse=reverse)
 
-    def areal(self, coords, anat_coords):
-        return areal(self, coords, anat_coords)
+    def areal(self, coords1, anat_coords, coords2=None):
+        return areal(self, coords1, anat_coords, coords2)
+
+    def areal_highres(self, coords1, anat_coords, coords2=None, n_div=4):
+        highres_sphere = self.subdivide(n_div)
+        highres_anat = surface_subdivision(anat_coords, self.faces, n_div)[0]
+        if coords2 is None:
+            mat = highres_sphere.areal(self.coords, highres_anat, coords1)
+        else:
+            mat = highres_sphere.areal(coords1, highres_anat, coords2)
+        return mat
 
     def union(self, to_unite, eps=1e-10):
         if isinstance(to_unite, Sphere):
