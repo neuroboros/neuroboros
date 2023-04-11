b12af3a4a3e2328590e86ad46f21370d078079a2
Feilong Ma <mafeilong@gmail.com>
2023-04-11 15:21:03 -0400

    Alternative data directory.

--- src/neuroboros/io.py
+++ src/neuroboros/io.py
@@ -5,7 +5,15 @@ from .utils import load
 
 DATA_ROOT = os.environ.get(
     'NEUROBOROS_DATA_DIR',
-    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'nb_data'))
+    os.path.join(os.path.expanduser('~'), '.neuroboros-data'))
+
+
+def get_core_dataset():
+    core_dir = os.path.join(DATA_ROOT, 'core')
+    dset = dl.clone(
+        source='https://gin.g-node.org/neuroboros/core',
+        path=core_dir)
+    return dset
 
 
 def _follow_symlink(fn, root):
@@ -26,14 +34,17 @@ def download_datalad_file(fn, dl_dset):
     return result['path']
 
 
-def load_file(fn, root=None):
+def load_file(fn, dset=None, root=None):
     if root is None:
-        root = DATA_ROOT
-    dset = dl.Dataset(root)
+        if dset is None:
+            dset = get_core_dataset()
+        root = dset.path
+    else:
+        dset = dl.Dataset(root)
     path = os.path.join(root, fn)
+    if not os.path.lexists(path):
+        return None
     if not (dset.repo is None):
         res = download_datalad_file(fn, dset)
-        assert os.path.normpath(path) == os.path.normpath(res['path'])
-    if not os.path.exists(path):
-        return None
+        assert os.path.normpath(path) == os.path.normpath(res)
     return load(path)
