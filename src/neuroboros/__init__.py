6a2716ec86aa5bacfdc538b124892ba76e24fd81
Feilong Ma <mafeilong@gmail.com>
2023-04-21 14:52:27 -0400

    Add API for mapping between spaces.

--- src/neuroboros/__init__.py
+++ src/neuroboros/__init__.py
@@ -9,6 +9,7 @@ from .spaces import get_mask as mask
 from .spaces import get_morphometry as morphometry
 from .spaces import get_parcellation as parcellation
 from .spaces import get_geometry as geometry
+from .spaces import get_mapping as mapping
 from .searchlights import get_searchlights as searchlights
 from .datasets import Bologna, Forrest, Dalmatians
 from .utils import save, save_results, load, percentile
