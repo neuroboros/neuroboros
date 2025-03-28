89049bb215fc86fb46a9a6ee851cda952893fc31
Feilong Ma <mafeilong@gmail.com>
2025-03-28 18:25:05 -0400

    Better assert with np.testing.assert_allclose.

--- tests/test_glm.py
+++ tests/test_glm.py
@@ -5,7 +5,7 @@ from neuroboros import glm
 
 
 def test_glm():
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(0)
     dm = rng.standard_normal((1000, 500))
     nuisance = rng.standard_normal((1000, 10))
     design = rng.standard_normal((1000, 10))
@@ -15,12 +15,12 @@ def test_glm():
     assert ts.shape == (10, 500)
     assert R2s.shape == (500,)
 
-    assert np.allclose(np.mean(R2s), 0, atol=0.001)
-    assert np.allclose(np.mean(betas), 0, atol=0.001)
+    np.testing.assert_allclose(np.mean(R2s), 0, atol=0.001)
+    np.testing.assert_allclose(np.mean(betas), 0, atol=0.001)
 
 
 def test_t_value():
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(0)
     dm = rng.standard_normal((100, 1))
     nuisance = None
     design = rng.choice([0, 1], (100, 1))
@@ -28,4 +28,4 @@ def test_t_value():
     ts = glm(dm, design, nuisance)[1]
     t = ttest_ind(dm[design[:, 0] == 1], dm[design[:, 0] == 0], equal_var=True)
     print(ts[0], t.statistic)
-    assert np.allclose(ts[0], t.statistic, atol=0.001)
+    np.testing.assert_allclose(ts[0], t.statistic, atol=0.001)
